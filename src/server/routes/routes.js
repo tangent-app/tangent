bbddd3d5dab137bcb4fe1aa673566f3669805491
jyoon006 <jyoon006@gmail.com>
2016-12-08 16:30:02 -0500

    code clean up

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -2,18 +2,17 @@ var helpers = require('../helpers/helpers');
 
 module.exports = function(app, express) {
   app.get('/api/signin', function(req, res) {
-    console.log('here');
     // var username = req.body.data.username;
     // var password = req.body.data.password;
     
-    var result = new Promise(function(resolve, reject) {
+    new Promise(function(resolve, reject) {
       resolve(helpers.signIn());
-    });
-
-    result.then(function(data) {
+    })
+    .then(function(data) {
       console.log('ressss', data);
       res.json(data);
     });
+    
   });
  
 }
